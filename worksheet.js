7ea885c524e29f467549d3683f82c869a7645303
Kyle-Wisecarver <wiseworks.2022@gmail.com>
2022-04-25 08:15:23 -0500

    Created for loop and variable to print to console the numnbers in range 0-10

--- worksheet.js
+++ worksheet.js
@@ -1,56 +1,61 @@
-let dayOfWeek = "Monday";
-console.log(dayOfWeek)
+//let dayOfWeek = "Monday";
+//console.log(dayOfWeek)
 
-dayOfWeek = 'Friday'
-console.log("I can't wait for " + dayOfWeek + "!")
+//dayOfWeek = 'Friday'
+//console.log("I can't wait for " + dayOfWeek + "!")
 
-////Create variable named animalInput
-let animalInput = prompt("What is your favorite animal?")
+//////Create variable named animalInput
+//let animalInput = prompt("What is your favorite animal?")
 
-////Create variable named colorInput
-let colorInput = prompt('What is your favorite color?')
+//////Create variable named colorInput
+//let colorInput = prompt('What is your favorite color?')
 
-//Combine these two variables within a string so you can print the phrase
-//"Ive never seen a <color><animal>!"
-console.log("I've never seen a " + colorInput + animalInput + "!")
-///
-///
-///Create variable named timeOfDay with value = 1300
-let timeOfDay = (1700) //array)
-// Create another variable, self named. This variable will hold the value
-// of one of the meals you have previously chosen. 
-let choices = ['Eggs', 'Sandwich', 'Steak']
-let choice = ''
-if (timeOfDay <= 1200) {
-    choice = 'Eggs'
-}
-else if (timeOfDay >= 1700) {
+////Combine these two variables within a string so you can print the phrase
+////"Ive never seen a <color><animal>!"
+//console.log("I've never seen a " + colorInput + animalInput + "!")
+/////
+/////
+/////Create variable named timeOfDay with value = 1300
+//let timeOfDay = (1700) //array)
+//// Create another variable, self named. This variable will hold the value
+//// of one of the meals you have previously chosen.
+//let choices = ['Eggs', 'Sandwich', 'Steak']
+//let choice = ''
+//if (timeOfDay <= 1200) {
+//    choice = 'Eggs'
+//}
+//else if (timeOfDay >= 1700) {
     
-    choice = 'Steak'
-}
-else if (timeOfDay > 1200 <= 1700) {
+//    choice = 'Steak'
+//}
+//else if (timeOfDay > 1200 <= 1700) {
 
-    choice = 'Sandwich'
-}
-console.log(choice)
+//    choice = 'Sandwich'
+//}
+//console.log(choice)
 
-randomNumber = Math.floor(Math.random() * 11)
-//while (randomNumber != 10) {
-    if (randomNumber >= 0 <= 2) {
-        console.log('Beatles')
-    }
-    else if (randomNumber >= 3 <= 5) {
-        console.log('Stones')
-    }
-    else if (randomNumber >= 6 <= 8) {
-        console.log('Floyd')
-    }
-    else if (randomNumber >= 9 <= 10) {
-        console.log('Hendrix')
-    }
+//randomNumber = Math.floor(Math.random() * 11)
+////while (randomNumber != 10) {
+//    if (randomNumber >= 0 <= 2) {
+//        console.log('Beatles')
+//    }
+//    else if (randomNumber >= 3 <= 5) {
+//        console.log('Stones')
+//    }
+//    else if (randomNumber >= 6 <= 8) {
+//        console.log('Floyd')
+//    }
+//    else if (randomNumber >= 9 <= 10) {
+//        console.log('Hendrix')
+//    }
 
+////}
+
+//for (let i = 1; i < 8; i++){
+//    console.log('JavaScript is cool!');
 //}
 
-for (let i = 1; i < 8; i++){
-    console.log('JavaScript is cool!');
+for (let i = 0; i < 11; i++){
+    generatedNumber= i 
+    console.log(generatedNumber)
 }
