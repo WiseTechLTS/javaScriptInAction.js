523329c82f2b0db236442e5574ebe49e58c01168
Kyle-Wisecarver <wiseworks.2022@gmail.com>
2022-04-25 07:27:47 -0500

    Created variable and three conditionals which will result in the proper output. Tested on console by changing the timeOfDay variable

--- worksheet.js
+++ worksheet.js
@@ -15,23 +15,23 @@
 //console.log("I've never seen a " + colorInput + animalInput + "!")
 ///
 ///
-///Favorite breakfast, lunch, dinner
-///
-/// Eggs, Sandwich, Steak
-///
 ///Create variable named timeOfDay with value = 1300
-let timeOfDay = 1300
+let timeOfDay = 1700
 // Create another variable, self named. This variable will hold the value
 // of one of the meals you have previously chosen. 
-let listOfMeals = ['Eggs', 'Sandwich', 'Steak'];
-let chosenMeal = ""
-if (timeOfDay > 1200); {
-    chosenMeal === "Eggs"
+let choices = ['Eggs', 'Sandwich', 'Steak']
+let choice = ''
+if (timeOfDay <= 1200) {
+    choice = 'Eggs'
 }
-if (timeOfDay >= 1200 <= 1700); {
-    chosenMeal = "Sandwich"
+else if (timeOfDay >= 1700) {
+    
+    choice = 'Steak'
 }
-if (timeOfDay >= 1700); {
-    chosenMeal = "Steak"
+
+else if (timeOfDay > 1200 <= 1700) {
+
+    choice = 'Sandwich'
 }
-console.log(chosenMeal)
+
+console.log(choice)
