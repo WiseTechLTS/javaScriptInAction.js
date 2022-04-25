0fbf221bb99ba54a55e97f0350e6f4abfde7b53f
Kyle-Wisecarver <wiseworks.2022@gmail.com>
2022-04-25 04:23:39 -0500

    Created user input for two distinct variables, combined them in the sam phrase and printed to console.

--- worksheet.js
+++ worksheet.js
@@ -3,4 +3,11 @@ console.log(dayOfWeek)
 
 dayOfWeek = 'Friday'
 
-console.log("I can't wait for " +dayOfWeek+"!")
+console.log("I can't wait for " + dayOfWeek + "!")
+//Create variable named animalInput
+let animalInput = prompt("What is your favorite animal?")
+//Create variable named colorInput 
+let colorInput = prompt('What is your favorite color?')
+//Combine these two variables within a string so you can print the phrase
+//"Ive never seen a <color><animal>!"
+console.log("I've never seen a "+ colorInput + animalInput + "!")
