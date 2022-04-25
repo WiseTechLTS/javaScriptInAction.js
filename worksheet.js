78ae65804c1f50455362fe54a456970793471c2c
Kyle-Wisecarver <wiseworks.2022@gmail.com>
2022-04-25 04:48:13 -0500

    Created three variable and three meals. Assinged variable values based on timeOfDay value.

--- worksheet.js
+++ worksheet.js
@@ -1,13 +1,37 @@
-let dayOfWeek = "Monday";
-console.log(dayOfWeek)
+//let dayOfWeek = "Monday";
+//console.log(dayOfWeek)
 
-dayOfWeek = 'Friday'
+//dayOfWeek = 'Friday'
+//console.log("I can't wait for " + dayOfWeek + "!")
 
-console.log("I can't wait for " + dayOfWeek + "!")
-//Create variable named animalInput
-let animalInput = prompt("What is your favorite animal?")
-//Create variable named colorInput 
-let colorInput = prompt('What is your favorite color?')
+////Create variable named animalInput
+//let animalInput = prompt("What is your favorite animal?")
+
+////Create variable named colorInput
+//let colorInput = prompt('What is your favorite color?')
+
 //Combine these two variables within a string so you can print the phrase
 //"Ive never seen a <color><animal>!"
-console.log("I've never seen a "+ colorInput + animalInput + "!")
+//console.log("I've never seen a " + colorInput + animalInput + "!")
+///
+///
+///Favorite breakfast, lunch, dinner
+///
+/// Eggs, Sandwich, Steak
+///
+///Create variable named timeOfDay with value = 1300
+let timeOfDay = 1300
+// Create another variable, self named. This variable will hold the value
+// of one of the meals you have previously chosen. 
+let listOfMeals = ['Eggs', 'Sandwich', 'Steak'];
+let chosenMeal = ""
+if (timeOfDay > 1200); {
+    chosenMeal === "Eggs"
+}
+if (timeOfDay >= 1200 <= 1700); {
+    chosenMeal = "Sandwich"
+}
+if (timeOfDay >= 1700); {
+    chosenMeal = "Steak"
+}
+console.log(chosenMeal)
